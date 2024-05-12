84e600aeed399147154d939dc76f1eed52555e98
G-nizam-A <nizamcs10@gmail.com>
2024-05-12 22:38:10 +0530

    new updates

--- assets/js/accounts.js
+++ assets/js/accounts.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   var title = "Accounts";
 
-  var data= [
+  var accountsData= [
     ["data1", "data2", "data3", "data4", "data5", "data6", "data7", "data8", "data9", "data10", "data11", "data12", "data13", "data14", "data15", "data16"],
     ["data1", "data2", "data3", "data4", "data5", "data6", "data7", "data8", "data9", "data10", "data11", "data12", "data13", "data14", "data15", "data16"],
     ["data1", "data2", "data3", "data4", "data5", "data6", "data7", "data8", "data9", "data10", "data11", "data12", "data13", "data14", "data15", "data16"],
@@ -39,13 +39,20 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   $('.dt-search label').remove();
-  data.forEach(function (row) {
+  accountsData.forEach(function (row) {
     var newRow = table.row.add(row);
     newRow.draw();
   });
 
-  var titleDiv = $(`<div class="dt-layout-cell dt-start"><div class="dt-length">${title}</div></div>`);
-  $('.dt-layout-row .dt-start').after(titleDiv);
+  var titleDiv = $(`<div class="dt-layout-cell dt-start"><h2 class="dt-length">${title}</h2></div>`);
+  $('.dt-layout-row .dt-start:first').after(titleDiv);
+
+// // Add title between dt-layout-cell dt-start and dt-layout-cell dt-end
+// var title = $('<h2>').text('Your Title Here'); // Create a new <h2> element with your title
+// var wrapper = $('<div>').addClass('dt-layout-cell dt-start'); // Create a wrapper div with class dt-layout-cell dt-start
+// wrapper.append(title); // Append the title to the wrapper div
+// $('#myTable_wrapper .dt-layout-row .dt-start').append(wrapper); // Prepend the wrapper div to the DataTable wrapper
+
 
 
 });
